cbdd7568331595f9f82840acceae990418291087
Richelly Italo <richellyitalo@gmail.com>
2022-10-01 21:56:38 -0300

    Set app component as class Component

--- 01_monster_rolodex/src/App.js
+++ 01_monster_rolodex/src/App.js
@@ -1,25 +1,41 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Richelly',
+      age: 18
+    };
+
+    this.changeTheName = this.changeTheName.bind(this)
+  }
+
+  changeTheName() {
+    // way one
+    this.setState({ name: 'Italo', age: this.state.age + 2 })
+
+    // way two
+    // this.setState(state => ({name: state.name + state.name}))
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hi My name is {this.state.name}.</p>
+          <p>And I have {this.state.age} years old.</p>
+          <button onClick={this.changeTheName}>
+            Change the Name!
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
