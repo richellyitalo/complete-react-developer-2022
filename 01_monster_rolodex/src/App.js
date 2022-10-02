08d0a024e3dd31f9d423df98db8654b30c57a582
Richelly Italo <richellyitalo@gmail.com>
2022-10-02 12:07:22 -0300

    01_MonsterRolode: optimizing state

--- 01_monster_rolodex/src/App.js
+++ 01_monster_rolodex/src/App.js
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,43 +6,88 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Richelly',
-      age: 18
+      monsters: [],
+      searchTerm: '',
     };
 
-    this.changeTheName = this.changeTheName.bind(this)
+    // this.changeTheName = this.changeTheName.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users/')
+      .then((res) => {
+        return res.json();
+      })
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            return console.log(this.state.monsters);
+          }
+        )
+      );
   }
 
   changeTheName() {
     // way one
     // most straightly
-    // this.setState({name: 'Italo'});
-
+    // this.setState({name: {firstName: 'Italo', lastName: 'Rich'}});
     // way two
     // just change directly the index using old value
     // this.setState(state => ({name: state.name + state.name}))
-
     // way three
     // return the final object after conditions
-    this.setState(state => {
-      let name = state.name === 'Richelly' ? 'Italo' : 'Richelly';
-      let age = state.age < 30 ? state.age + 1 : 18;
-            
-      return {name, age};
-    })
+    // this.setState(state => {
+    //   console.log('change');
+    //   let name = state.name === 'Richelly' ? 'Italo' : 'Richelly';
+    //   let age = state.age < 30 ? state.age + 1 : 18;
+    //   return {name, age};
+    // });
+    // way four
+    // with callback
+    // this.setState(
+    //   () => {
+    //     return {
+    //       name: {
+    //         firstName: 'John',
+    //         lastName: 'Doo',
+    //       },
+    //     };
+    //   },
+    //   () => {
+    //     // show the updated state
+    //     console.log(this.state);
+    //   }
+    // );
   }
 
+  onSearchChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
   render() {
+    const { monsters, searchTerm } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter(
+      (monster) =>
+        searchTerm.length === 0 ||
+        monster.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi My name is {this.state.name}.</p>
-          <p>And I have {this.state.age} years old.</p>
-          <button onClick={() => this.changeTheName()}>
-            Change the Name!
-          </button>
-        </header>
+        <input
+          type="search"
+          className="input-search"
+          autoFocus={true}
+          onChange={onSearchChange}
+        />
+        {filteredMonsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
