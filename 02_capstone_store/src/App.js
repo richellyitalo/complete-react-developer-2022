80e836ccd469ca8c4783a7180c669d692287b08a
Richelly Italo <richellyitalo@gmail.com>
2022-10-19 21:13:37 -0300

    feat: navigation component with outlet

--- 02_capstone_store/src/App.js
+++ 02_capstone_store/src/App.js
@@ -1,12 +1,18 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
+import Navigation from './components/navigation/navigation.component';
 
-const Other = () => <h3>Other</h3>;
+const Shop = () => {
+  return <h3>here is shop</h3>
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
