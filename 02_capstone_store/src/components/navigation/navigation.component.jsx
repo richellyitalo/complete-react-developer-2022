1886f9d090b6a0c62bbd52a281b435225b124d5b
Richelly Italo <richellyitalo@gmail.com>
2022-10-19 21:31:20 -0300

    feat: add logo to navigation

--- 02_capstone_store/src/components/navigation/navigation.component.jsx
+++ 02_capstone_store/src/components/navigation/navigation.component.jsx
@@ -1,16 +1,21 @@
 import { Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/images/shopping-logo.svg';
+
+import './navigation.styles.scss';
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
-        <Link to="/">Logo</Link>
-      </div>
-      <div className="nav-links-container">
-        <Link className="nav-link" to="/shop">
-          SHOP
+        <Link className="logo-container" to="/">
+          <Logo className="logo" />
         </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
       </div>
 
       <Outlet />
