64333a9ff803a622a6b5dd298c1333b70e7e28a2
Richelly Italo <richellyitalo@gmail.com>
2022-10-30 09:58:32 -0300

    chore: example env

--- 02_capstone_store/src/utils/firebase/firebase.utils.js
+++ 02_capstone_store/src/utils/firebase/firebase.utils.js
@@ -7,14 +7,14 @@ import {
 } from 'firebase/auth';
 
 import { doc, getDoc, setDoc, getFirestore } from 'firebase/firestore';
-
+console.log(process.env);
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
